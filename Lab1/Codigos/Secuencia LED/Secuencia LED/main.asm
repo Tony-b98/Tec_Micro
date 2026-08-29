b7f0504bb4a4271bff8faab1c368337c63ccd218
santiagomoizo <santiago.moizo@estudiantes.utec.edu.uy>
2026-08-29 02:18:31 -0300

    Configuracion de botones

--- Lab1/Codigos/Secuencia LED/Secuencia LED/main.asm
+++ Lab1/Codigos/Secuencia LED/Secuencia LED/main.asm
@@ -36,3 +36,109 @@ RESET:
 
     clr secuencia
     rcall REINICIAR_SECUENCIA
+
+MAIN_LOOP:
+    rcall CHECK_BOTONES
+    rcall MOSTRAR_PATRON
+    rcall DELAY_PASO
+    rcall EJECUTAR_PASO
+    rjmp MAIN_LOOP
+
+CHECK_BOTONES:
+
+    ; Boton siguiente
+    sbis PINB, BTN_SIG
+    rcall MANEJAR_SIGUIENTE
+
+    ; Boton anterior
+    sbis PINB, BTN_ANT
+    rcall MANEJAR_ANTERIOR
+
+    ; Boton inicio
+    sbis PINB, BTN_INICIO
+    rcall MANEJAR_INICIO
+
+    ret
+
+MANEJAR_SIGUIENTE:
+    rcall DELAY_DEBOUNCE
+
+    ; Confirmar que sigue presionado
+    sbis PINB, BTN_SIG
+    rjmp SIG_CONFIRMADO
+
+    ret
+
+SIG_CONFIRMADO:
+    inc secuencia
+
+    ; Ahora hay 8 secuencias: 0,1,2,3,4,5,6,7
+    cpi secuencia, 8
+    brne SIG_OK
+
+    ; Si llega a 8 vuelve a 0
+    clr secuencia
+
+SIG_OK:
+    rcall REINICIAR_SECUENCIA
+
+SIG_ESPERA:
+    ; Esperar a que se suelte el boton
+    sbis PINB, BTN_SIG
+    rjmp SIG_ESPERA
+
+    rcall DELAY_DEBOUNCE
+    ret
+
+MANEJAR_ANTERIOR:
+    rcall DELAY_DEBOUNCE
+
+    ; Confirmar pulsacion
+    sbis PINB, BTN_ANT
+    rjmp ANT_CONFIRMADO
+
+    ret
+
+ANT_CONFIRMADO:
+
+    ; Si estamos en 0, pasar a 7
+    cpi secuencia, 0
+    brne ANT_DEC
+
+    ldi secuencia, 8
+
+ANT_DEC:
+    dec secuencia
+
+    rcall REINICIAR_SECUENCIA
+
+ANT_ESPERA:
+    ; Esperar a soltar el boton
+    sbis PINB, BTN_ANT
+    rjmp ANT_ESPERA
+
+    rcall DELAY_DEBOUNCE
+    ret
+
+MANEJAR_INICIO:
+    rcall DELAY_DEBOUNCE
+
+    ; Confirmar pulsacion
+    sbis PINB, BTN_INICIO
+    rjmp INICIO_CONFIRMADO
+
+    ret
+
+INICIO_CONFIRMADO:
+
+    ; Volver a la primera secuencia
+    clr secuencia
+
+    rcall REINICIAR_SECUENCIA
+
+INICIO_ESPERA:
+    sbis PINB, BTN_INICIO
+    rjmp INICIO_ESPERA
+
+    rcall DELAY_DEBOUNCE
+    ret
