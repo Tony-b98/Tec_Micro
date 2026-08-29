f4dadceaca5db664f02a88f8e2dc3915d14a72e8
santiagomoizo <santiago.moizo@estudiantes.utec.edu.uy>
2026-08-28 21:17:32 -0300

    Aux 3 falta y configuracion de RESET

--- Lab1/Codigos/Secuencia LED/Secuencia LED/main.asm
+++ Lab1/Codigos/Secuencia LED/Secuencia LED/main.asm
@@ -1,11 +1,38 @@
 .include "m328Pdef.inc"
 
 .def temp      = r16
-.def secuencia = r17     ; 0-3, secuencia actualmente activa
-.def patron    = r18     ; patrón de 8 bits que se muestra en los LEDs
+.def secuencia = r17
+.def patron    = r18
 .def aux       = r21
 .def aux2      = r22
+.def aux3      = r23
 
-.equ BTN_SIG    = 0   ; PB0
-.equ BTN_ANT    = 1   ; PB1
-.equ BTN_INICIO = 2   ; PB2
+.equ BTN_SIG    = 2
+.equ BTN_ANT    = 3
+.equ BTN_INICIO = 4
+
+.org 0x0000
+    rjmp RESET
+
+RESET:
+
+    ldi temp, low(RAMEND)
+    out SPL, temp
+
+    ldi temp, high(RAMEND)
+    out SPH, temp
+
+
+    ldi temp, 0b11111100
+    out DDRD, temp
+
+    ldi temp, 0b00000011
+    out DDRB, temp
+
+
+    ldi temp, 0b00011100
+    out PORTB, temp
+
+
+    clr secuencia
+    rcall REINICIAR_SECUENCIA
