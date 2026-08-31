4b705802808cd05c4105e1c339391a59719a83a4
Nicolas Romero <sergio.romero@estudiantes.utec.edu.uy>
2026-08-30 21:09:53 -0300

    Actualizo codigo del ejercicio C

--- Lab1/Codigos/EjC. Secuencia LED/Secuencia LED/main.asm
+++ Lab1/Codigos/EjC. Secuencia LED/Secuencia LED/main.asm
@@ -13,12 +13,15 @@
 .equ BTN_ANT    = 3
 .equ BTN_INICIO = 4
 
+; Segmento de memoria de programa
+.cseg
+
 ; Vector de reset
 .org 0x0000
     rjmp RESET
 
 RESET:
-	;Inicio de SP en el ultimo lugar de la RAM
+	;Inicio de SP en el último lugar de la RAM
     ldi temp, low(RAMEND)
     out SPL, temp
 	ldi temp, high(RAMEND)
@@ -178,3 +181,118 @@ RS_OUT:
     rcall MOSTRAR_PATRON
     ret
 
+;Ejecutar paso de la secuencia actual.
+
+EJECUTAR_PASO:
+
+    cpi secuencia, 0
+    breq PASO_SEQ0
+
+    cpi secuencia, 1
+    breq PASO_SEQ1
+
+    cpi secuencia, 2
+    breq PASO_SEQ2
+
+    cpi secuencia, 3
+    breq PASO_SEQ3
+
+    cpi secuencia, 4
+    breq PASO_SEQ4
+
+    cpi secuencia, 5
+    breq PASO_SEQ5
+
+    cpi secuencia, 6
+    breq PASO_SEQ6
+
+    rjmp PASO_SEQ7
+
+
+; SECUENCIA 0
+; Un led dezplazándse en un sentido
+PASO_SEQ0:
+    lsl patron
+    brne PS0_OUT
+
+    ldi patron, 0x01
+
+PS0_OUT:
+    ret
+
+; SECUENCIA 1
+; Un led dezlasándose en sentido contrario
+PASO_SEQ1:
+    lsr patron
+    brne PS1_OUT
+
+    ldi patron, 0x80
+
+PS1_OUT:
+    ret
+
+; SECUENCIA 2
+; LEDs ALTERNADOS
+PASO_SEQ2:
+    com patron
+    ret
+
+; SECUENCIA 3
+; LLENADO PROGRESIVO
+PASO_SEQ3:
+
+; Si ya estan todos encendidos,
+; comenzar nuevamente
+    cpi patron, 0xFF
+    breq PS3_CLEAR
+
+    lsl patron
+    ori patron, 0x01
+    ret
+
+PS3_CLEAR:
+    clr patron
+    ret
+
+; SECUENCIA 4
+; PATRON 00010001 DESPLAZANDOSE
+
+PASO_SEQ4:
+
+    cpi patron, 0x88
+    breq PS4_REINICIAR
+
+    lsl patron
+    ret
+
+
+PS4_REINICIAR:
+    ldi patron, 0x11
+    ret
+
+; SECUENCIA 5
+; DOS LEDs JUNTOS DESPLAZANDOSE
+PASO_SEQ5:
+
+    cpi patron, 0xC0
+    breq PS5_REINICIAR
+
+    lsl patron
+    ret
+
+
+PS5_REINICIAR:
+    ldi patron, 0x03
+    ret
+
+; SECUENCIA 6
+; TODOS LOS LEDs PARPADEAN
+PASO_SEQ6:
+    com patron
+    ret
+
+; SECUENCIA 7
+; MITAD IZQUIERDA / MITAD DERECHA
+PASO_SEQ7:
+    com patron
+    ret
