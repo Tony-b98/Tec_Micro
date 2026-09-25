d2232b73c92124b12fd37707eda40cd197941b3d
santiagomoizo <santiago.moizo@estudiantes.utec.edu.uy>
2026-09-25 19:32:38 -0300

    Codigo Micro 2

--- Lab2/Codigos/ParteD/Comunicación USART UART MICRO2/Comunicación USART UART MICRO2/main.asm
+++ Lab2/Codigos/ParteD/Comunicación USART UART MICRO2/Comunicación USART UART MICRO2/main.asm
@@ -1,12 +1,41 @@
-;
-; Comunicación USART UART MICRO2.asm
-;
-; Created: 25/9/2026 19:15:49
-; Author : Santiago
-;
+.include "m328Pdef.inc"
 
+; Definicion de registros
+.def temp = r16
+.def dato = r17
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+; RESET
+.cseg
+.org 0x0000
+    rjmp RESET
+
+RESET:
+    ;Inicializo SP
+    ldi temp, low(RAMEND)
+    out SPL, temp
+
+    ldi temp, high(RAMEND)
+    out SPH, temp
+
+    ; PB0..PB5 salidas (LED 0..5)
+    ldi temp, 0b00111111
+    out DDRB, temp
+
+    ; PC0..PC1 salidas (LED 6..7)
+    ldi temp, 0b00000011
+    out DDRC, temp
+
+    clr temp
+    out PORTB, temp
+    out PORTC, temp
+
+    rcall USART_INIT
+
+; MAIN
+MAIN:
+    ; Espera dato del micro 1
+    rcall USART_RX
+
+	andi dato, 0x07
+
+	rcall APAGAR_LEDS
