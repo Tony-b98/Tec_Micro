fe2e2d1f8c1f2dd41ccf661b98f3569f926cb448
Anthony Bonino <anthony.bonino@estudiantes.utec.edu.uy>
2026-09-25 20:45:26 -0300

    Guia de seleccion de led correspondiente

--- Lab2/Codigos/ParteD/Comunicación USART UART MICRO2/Comunicación USART UART MICRO2/main.asm
+++ Lab2/Codigos/ParteD/Comunicación USART UART MICRO2/Comunicación USART UART MICRO2/main.asm
@@ -35,7 +35,74 @@ RESET:
 MAIN:
     ; Espera dato del micro 1
     rcall USART_RX
-
 	andi dato, 0x07
-
 	rcall APAGAR_LEDS
+
+    ; Seleccion de LED correspondiente
+    
+    cpi dato, 0
+    breq LED_0
+
+    cpi dato, 1
+    breq LED_1
+
+    cpi dato, 2
+    breq LED_2
+
+    cpi dato, 3
+    breq LED_3
+
+    cpi dato, 4
+    breq LED_4
+
+    cpi dato, 5
+    breq LED_5
+
+    cpi dato, 6
+    breq LED_6
+
+    cpi dato, 7
+    breq LED_7
+
+    rjmp MAIN
+
+
+; LED 0 -> D8 / PB0
+LED_0:
+    sbi PORTB, 0
+    rjmp MAIN
+
+; LED 1 -> D9 / PB1
+LED_1:
+    sbi PORTB, 1
+    rjmp MAIN
+
+; LED 2 -> D10 / PB2
+LED_2:
+    sbi PORTB, 2
+    rjmp MAIN
+
+; LED 3 -> D11 / PB3
+LED_3:
+    sbi PORTB, 3
+    rjmp MAIN
+
+; LED 4 -> D12 / PB4
+LED_4:
+    sbi PORTB, 4
+    rjmp MAIN
+
+; LED 5 -> D13 / PB5
+LED_5:
+    sbi PORTB, 5
+    rjmp MAIN
+
+; LED 6 -> A0 / PC0
+LED_6:
+    sbi PORTC, 0
+    rjmp MAIN
+
+; LED 7 -> A1 / PC1
+LED_7:
+    sbi PORTC, 1
+    rjmp MAIN
